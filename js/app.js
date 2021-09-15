4f58ab405a5214549f5fe0758dbdb6a0772010c3
malt0533 <malt0533@stud.kea.dk>
2021-09-15 17:38:06 +0200

    Filtrering m. checkbox done

--- js/app.js
+++ js/app.js
@@ -23,9 +23,37 @@ const lukBtn = document.querySelector(".luk");
 const recipe = document.querySelector(".recipe");
 const btns = document.querySelectorAll(".filter_btn_container button");
 
+const alleCheck = document.querySelector("check_1");
+const vodkaCheck = document.querySelector(".check_2");
+const romCheck = document.querySelector(".check_3");
+const ginCheck = document.querySelector(".check_4");
+const whiskeyCheck = document.querySelector(".check_5");
+const tequilaCheck = document.querySelector(".check_6");
+const campariCheck = document.querySelector(".check_7");
+const conacCheck = document.querySelector(".check_8");
+const checkBox = document.querySelectorAll('input[type="checkbox"]');
+
 // Variabler
+let cocktails;
 const images = "../assets/images/cocktails/";
 let filter = "alle";
+
+let romBool, ginBool, whiskeyBool, tequilaBool, campariBool;
+vodkaBool = true;
+romBool = false;
+ginBool = false;
+whiskeyBool = false;
+tequilaBool = false;
+campariBool = false;
+conacBool = false;
+
+vodkaCheck.checked = vodkaBool;
+romCheck.checked = romBool;
+ginCheck.checked = ginBool;
+whiskeyCheck.checked = whiskeyBool;
+tequilaCheck.checked = tequilaBool;
+campariCheck.checked = campariBool;
+conacCheck.checked = conacBool;
 
 window.addEventListener("DOMContentLoaded", start);
 
@@ -45,24 +73,42 @@ async function loadJSON() {
 	cocktails = await JSONData.json();
 	console.log(cocktails);
 
-	vis(cocktails);
+	vis();
 }
 
-function vis(json) {
+function vis() {
+	checkBox.forEach((el) => {
+		el.addEventListener("click", checkBoxHandler);
+	});
+
+	vodkaBool = vodkaCheck.checked;
+	romBool = romCheck.checked;
+	ginBool = ginCheck.checked;
+	whiskeyBool = whiskeyCheck.checked;
+	tequilaBool = tequilaCheck.checked;
+	campariBool = campariCheck.checked;
+	conacBool = conacCheck.checked;
+
 	const cocktailTemplate = document.querySelector("template");
 	const container = document.querySelector(".produkt_visning");
 	container.textContent = "";
 
-	json.forEach((el) => {
+	cocktails.forEach((el) => {
+		filter = el.alkohol_type;
+
 		let klon = cocktailTemplate.cloneNode(true).content;
 		klon.querySelector(".name").textContent = el.name;
 
-		if (filter == el.alkohol_type || filter == "alle") {
+		if (
+			(filter == "vodka" && vodkaBool === true) ||
+			(filter == "rom" && romBool === true) ||
+			(filter == "gin" && ginBool === true) ||
+			(filter == "whiskey" && whiskeyBool === true) ||
+			(filter == "tequila" && tequilaBool === true) ||
+			(filter == "campari" && campariBool === true) ||
+			(filter == "conac" && conacBool === true)
+		) {
 			klon.querySelector("img").src = `${images}${el.billede_navn}.jpg`;
-			// klon.querySelector(".kort").textContent = el.kortbeskrivelse;
-			// klon.querySelector("img").src = `${images}${el.billednavn}-md.jpg`;
-			// klon.querySelector(".country").textContent = `Land: ${el.oprindelsesregion}`;
-			// klon.querySelector(".pris-tag").textContent = `${el.pris} kr.`;
 
 			// Vis Pop-up
 			klon.querySelector(".produkt").addEventListener("click", () => {
@@ -74,7 +120,6 @@ function vis(json) {
 	});
 }
 
-// TODO Filtrering - Med checkbox
 function filtrerCocktails() {
 	filter = this.dataset.alkohol;
 	document.querySelector(".active_filter").classList.remove("active_filter");
@@ -114,7 +159,6 @@ function showSinglePage(cocktail) {
 
 // Scroll funktion
 let bg_pattern = document.querySelector(".background");
-
 // Hver gang der bliver scrollet kører denne funktion
 window.addEventListener("scroll", () => {
 	// Tager fat i vinduets y-position
@@ -122,3 +166,8 @@ window.addEventListener("scroll", () => {
 	// Manipulere positionen for baggrunds mønstret i forhold til scrolling
 	bg_pattern.style.transform = `translateY(${scroll * 0.5}px)`;
 });
+
+function checkBoxHandler() {
+	this.checked ? (this.checked = true) : (this.checked = false);
+	vis();
+}
